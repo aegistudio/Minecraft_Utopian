2524279b109d151c80d3c1972c97665a42bd164a
aegistudio <1910567646@qq.com>
2015-07-05 18:44:16 +0800

    .

--- run-client.bat
+++ run-client.bat
@@ -1,3 +1,3 @@
 @echo off
-javaw -cp bin;json;soundsystem;lib\\library_collections.jar;lib\\minecraft.jar net.aegistudio.minecraft.utopian.Client
+javaw -cp bin;json;soundsystem;lib\\library_collections.jar;lib\\minecraft.jar net.aegistudio.minecraft.utopian.Client 
 pause
---
+++ run-client.sh
@@ -0,0 +1,1 @@
+java -cp bin:json:soundsystem:lib/library_collections.jar:lib/minecraft.jar -Djava.library.path=natives/linux net.aegistudio.minecraft.utopian.Client 
